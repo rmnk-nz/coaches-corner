7842079efbc8aed0580e3548a272caeca5d57a83
rmnk-nz <rua.manukau20@gmail.com>
2022-01-28 15:53:02 +1100

    updated packages and App

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+import User from './pages/User';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+    <Router>
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/admin' component={Admin} />
+          <Route exact path='/user' component={User} />
+        </Switch>
+      </>
+    </Router>
+  </ApolloProvider>
   );
 }
 
