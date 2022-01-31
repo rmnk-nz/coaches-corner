4e79eedbd3d114adce56583b8d2c22961142b787
rmnk-nz <rua.manukau20@gmail.com>
2022-01-31 12:12:44 +1100

    added form to retrieve programme data

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../styles/Admin.css';
 
 function Admin() {
-  return <div>
-      this is admin
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      const programData = { title, body };
+      console.log(programData);
+  };
+
+  return <div className='admin'>
+        <h3 className='welcome'>Welcome Back Coach!!</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Add Your Programming</label>
+        <input type='text' 
+          required 
+          value={title} 
+          onChange={(e) => setTitle(e.target.value)} 
+        />
+        <textarea required
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+        />
+        <div>
+          <button>SAVE</button>
+        </div>
+      </form>
+      <h3 className='welcome'>Programming History:</h3>
   </div>;
 };
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,7 +12,7 @@ function Home() {
     <img className='homeImage' src={homeImage} alt='homepageImage'/>
     <div className='equip'>
         <h3>What Equipment Will I Need?</h3>
-        <p>**NOTE: For maximum progression with this program, you will need access to a full range of gym equipment. Mimimun equipment requiremnts listed below:</p>
+        <p>**NOTE: For maximum progression with this program, you will need access to a full range of gym equipment. Minimum equipment requiremnts below:</p>
         <p>
           Olympic Barbel - 90kg total weight plates - Squat Rack - Bench - Rubber Hex Dumbells (M x2 15kg - F x2 12.5kg) - Kettlebell (M 20kg - F 15kg) - Rubber Gym Flooring - Plyo Box - Speed Rope - Pull Up Rig - Rower 
         </p>
