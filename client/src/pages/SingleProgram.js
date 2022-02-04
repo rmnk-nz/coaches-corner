da931d4d12eca7bc96b9f79cd02f206fe1cdcda9
rmnk-nz <rua.manukau20@gmail.com>
2022-02-04 14:40:17 +1100

    added go back button and styles

--- client/src/pages/SingleProgram.js
+++ client/src/pages/SingleProgram.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../styles/SingleProgram.css'
 
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import { QUERY_PROGRAM } from '../utils/queries';
 
 const SingleProgram = () => {
+    // const location = useLocation();
+    const history = useHistory();
 
     const { programId } = useParams();
     const { loading, data } = useQuery(QUERY_PROGRAM, {
@@ -18,12 +20,18 @@ const SingleProgram = () => {
     if (loading) {
         return <div>...loading</div>
     }
-    return (
+    return (<>
+        <button
+        className='goBack'
+        onClick={() => history.goBack()}
+        >
+         &larr; Go Back
+       </button>
         <div className='singleProgramDiv'>
             <h3>{program.title}</h3>
             {program.body.split("\n").map((str, i) => <p key= {i}>{str}</p>)}
         </div>
-    )
+    </>)
 }
 
 export default SingleProgram;
